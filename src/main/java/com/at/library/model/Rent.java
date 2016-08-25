c1ed9231ee1d2eb826977f44b009b5a4f579a7d1
Dani Peralta <dani.peralta.de@gmail.com>
2016-08-25 14:57:27 +0200

    Añadida la clave embebida en los alquileres

--- src/main/java/com/at/library/model/Rent.java
+++ src/main/java/com/at/library/model/Rent.java
@@ -3,11 +3,9 @@ package com.at.library.model;
 import java.io.Serializable;
 import java.util.Date;
 
+import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
@@ -16,13 +14,9 @@ public class Rent implements Serializable {
 
 	private static final long serialVersionUID = -4158742374158942716L;
 
-	@Id
-	@GeneratedValue
-	private Integer id;
-
-	@OneToOne
-	private Book book;
-
+	@EmbeddedId
+	private RentPK rentpk;
+	
 	@ManyToOne
 	private User user;
 
@@ -30,42 +24,15 @@ public class Rent implements Serializable {
 	private Employee employee;
 	
 	@Temporal(TemporalType.TIMESTAMP)
-	private Date startDate;
-	
-	@Temporal(TemporalType.TIMESTAMP)
 	private Date endDate;
 	
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-
-	public Book getBook() {
-		return book;
-	}
-
-	public void setBook(Book book) {
-		this.book = book;
-	}
-
+	
 	public User getUser() {
 		return user;
 	}
 
 	public void setUser(User user) {
 		this.user = user;
-	}
-
-	public Date getStartDate() {
-		return startDate;
-	}
-
-	public void setStartDate(Date startDate) {
-		this.startDate = startDate;
 	}
 	
 	public Employee getEmployee() {
@@ -84,4 +51,13 @@ public class Rent implements Serializable {
 		this.endDate = endDate;
 	}
 
+	public RentPK getRentpk() {
+		return rentpk;
+	}
+
+	public void setRentpk(RentPK rentpk) {
+		this.rentpk = rentpk;
+	}
+
+	
 }

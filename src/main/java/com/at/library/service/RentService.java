c6e08dc8dc160fad3e335d8dd5077002ecaa83a2
root <root@dani-mint>
2016-08-28 14:01:18 +0200

    Comenzado el servicio para los alquileres

---
+++ src/main/java/com/at/library/service/RentService.java
@@ -0,0 +1,56 @@
+package com.at.library.service;
+
+import java.util.List;
+
+import com.at.library.dto.RentPostDTO;
+import com.at.library.model.Rent;
+
+
+public interface RentService {
+	
+	/**
+	 * Realiza la busqueda de todos los alquileres existentes
+	 * 
+	 * @return listado de alquileres
+	 */
+	List<RentPostDTO> findAll();
+
+	/**
+	 * Transforma un alquiler en un rentDTO
+	 * 
+	 * @param rent
+	 * @return
+	 */
+	RentPostDTO transform(Rent rent);
+
+	/**
+	 * Transforma un rentDTO en un alquiler
+	 * 
+	 * @param rentDTO
+	 * @return
+	 */
+	Rent transform(RentPostDTO rentDTO);
+
+	/**
+	 * Crea un alquiler
+	 * @param book
+	 * @return
+	 */
+	RentPostDTO create(RentPostDTO rentDTO);
+
+	/**
+	 * Busca por id
+	 * @param id
+	 * @return
+	 */
+	RentPostDTO findById(Integer id);
+
+	/**
+	 * Actualiza alquiler
+	 * @param book
+	 * @return
+	 */
+	void update(RentPostDTO bookDTO);
+	
+
+}
---
+++ src/main/java/com/at/library/service/RentServiceImpl.java
@@ -0,0 +1,49 @@
+package com.at.library.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.at.library.dto.RentPostDTO;
+import com.at.library.model.Rent;
+
+@Service
+public class RentServiceImpl implements RentService{
+
+	@Override
+	public List<RentPostDTO> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public RentPostDTO transform(Rent rent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Rent transform(RentPostDTO rentDTO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public RentPostDTO create(RentPostDTO rentDTO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public RentPostDTO findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(RentPostDTO bookDTO) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
